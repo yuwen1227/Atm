48c845531c8ae92a469a025f39b491d5d8db69b2
yuwen1227 <wen871227>
2018-10-30 15:38:07 +0800

    RC_LOGIN

--- app/src/main/java/com/wen/atm/LoginActivity.java
+++ app/src/main/java/com/wen/atm/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.wen.atm;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,6 +21,15 @@ public class LoginActivity extends AppCompatActivity {
         if ("yuwen1227".equals(userid) && "8787".equals(password)){
             setResult(RESULT_OK);
             finish();
+          new AlertDialog.Builder(this).setMessage("error")
+                  .setTitle("Error!!!!")
+                  .setMessage("Your Username or Password is incorrect")
+                  .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                      @Override
+                      public void onClick(DialogInterface dialog, int which) {
+
+                      }
+                  })
         }
 
     }
--- app/src/main/java/com/wen/atm/MainActivity.java
+++ app/src/main/java/com/wen/atm/MainActivity.java
@@ -3,8 +3,10 @@ package com.wen.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_LOGIN = 8787;
     boolean logon = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if(!logon){
             Intent intent = new Intent(this,LoginActivity.class);
-            startActivityForResult(intent,100);
+            startActivityForResult(intent,RC_LOGIN);
+
 
         }
     }
@@ -20,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 100){
+        if(requestCode == RC_LOGIN){
             if(resultCode !=RESULT_OK){
                 finish();
             }
