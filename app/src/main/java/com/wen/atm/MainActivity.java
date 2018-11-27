8029e50e8318554fd8cdbb926c3f2b543ef84463
yuwen1227 <coco4522@gmail.com>
2018-11-27 16:32:50 +0800

    recycler

--- app/src/main/java/com/wen/atm/MainActivity.java
+++ app/src/main/java/com/wen/atm/MainActivity.java
@@ -3,6 +3,8 @@ package com.wen.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -14,14 +16,22 @@ import java.util.List;
 public class MainActivity extends BaseActivity {
     private static final int RC_LOGIN = 8787;
     boolean logon = false;
+    List<String> fruits = Arrays.asList("香蕉","蘋果","草莓");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(!logon){
-            Intent intent = new Intent(this,LoginActivity.class);
-            startActivityForResult(intent,RC_LOGIN);
+        if (!logon) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivityForResult(intent, RC_LOGIN);
         }
+        //ListView
+        //listView();
+        //RecyclerView(可儲存資料)
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));//行列式清單
+        //recyclerView.setAdapter(new FruitAdapter());//在class新建一個class FruitAdapter 繼承RecyclerView
         //data
         List<String> fruits = Arrays.asList("香蕉","蘋果","草莓");
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
