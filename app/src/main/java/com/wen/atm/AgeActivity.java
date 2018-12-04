ce3ad64a7ed06638c7ebe64e150e46a6a0fc9b14
yuwen1227 <coco4522@gmail.com>
2018-12-04 16:38:25 +0800

    AgeAdapter + AgeHolder

--- app/src/main/java/com/wen/atm/AgeActivity.java
+++ app/src/main/java/com/wen/atm/AgeActivity.java
@@ -1,17 +1,28 @@
 package com.wen.atm;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class AgeActivity extends BaseActivity {
     EditText edage;
+    int[] numbers = {19,20,21,22,23,24,25};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new AgeAdapter());
     }
     public void next(View view){
         int age =  Integer.parseInt(((EditText)findViewById(R.id.ed_age)).getText().toString());
@@ -21,5 +32,36 @@ public class AgeActivity extends BaseActivity {
     }
     public void back(View view){
         finish();
+    }
+    class AgeAdapter extends RecyclerView.Adapter<AgeAdapter.AgeHolder>{
+
+        @NonNull
+        @Override
+        public AgeAdapter.AgeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.age_row,parent,false);
+            return new AgeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull AgeAdapter.AgeHolder holder, int position) {
+            holder.ageText.setText(numbers[position]+" ");
+            if(numbers[position] == 19){
+                holder.ageText.setTextColor(Color.BLUE);
+
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return numbers.length;
+        }
+
+        class AgeHolder extends RecyclerView.ViewHolder{
+                 TextView ageText;
+                 public AgeHolder(View itemView) {
+                     super(itemView);
+                     ageText = itemView.findViewById(R.id.tv_age);
+                 }
+             }
     }
 }
