e14099f94e84fed08a2fe7a24afb8c18fb47fb72
yuwen1227 <coco4522@gmail.com>
2018-12-18 16:30:45 +0800

    color

--- app/src/main/java/com/wen/atm/AgeActivity.java
+++ app/src/main/java/com/wen/atm/AgeActivity.java
@@ -7,22 +7,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+
 public class AgeActivity extends BaseActivity {
     EditText edage;
-    int[] numbers = {19,20,21,22,23,24,25};
+    int[] numbers = {18,19,20,21,22,23,24,25,26,27,28,29,30};
+    String[]rainbow = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+        rainbow = getResources().getStringArray(R.array.rainbow);
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new AgeAdapter());
+        //set Adapter
+        AgeAdapter adapter = new AgeAdapter();
+        recyclerView.setAdapter(adapter);
+        edage = findViewById(R.id.ed_age);
     }
     public void next(View view){
         int age =  Integer.parseInt(((EditText)findViewById(R.id.ed_age)).getText().toString());
@@ -43,12 +52,21 @@ public class AgeActivity extends BaseActivity {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull AgeAdapter.AgeHolder holder, int position) {
+        public void onBindViewHolder(@NonNull AgeAdapter.AgeHolder holder, final int position) {
             holder.ageText.setText(numbers[position]+" ");
+            holder.itemView.setBackgroundColor(Color.parseColor(rainbow[position%7]));
             if(numbers[position] == 19){
                 holder.ageText.setTextColor(Color.BLUE);
-
             }
+            // itemView 使用者點擊周遭區塊也可找到數字
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("Age","OnClick:"+ numbers[position]);
+                    edage.setText(numbers[position]+"");
+
+                }
+            });
         }
 
         @Override
